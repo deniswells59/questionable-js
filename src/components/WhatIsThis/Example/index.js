6429a5dfcd1160a9f75869a4a3813f1a4f05070b
Denis Wells <deniswells59@gmail.com>
2020-07-08 13:38:45 -0700

    working live edit with object type prop

--- src/components/WhatIsThis/Example/index.js
+++ src/components/WhatIsThis/Example/index.js
@@ -1,24 +1,56 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-import stringifyProp from '../../../utils/stringifyProp';
+import stringifyPropValue from '../../../utils/stringifyProp';
 
 const Example = ({ subject, exampleChildren, exampleProps }) => {
+  const [props, setProps] = useState(exampleProps);
   const Subject = subject;
 
   const displayActiveProps = () => {
     // Reduces each React prop into one string
     // Uses `stringifyProp` to handle prop values
-    return Object.keys(exampleProps).reduce((acc, propName) => {
-      return `${acc}   ${propName}={${stringifyProp(exampleProps[propName])}}\n`;
-    }, '');
+
+    const propsAsInputs = Object.keys(props).map((key) => {
+      return (
+        <>
+          {`  ${key}=`}
+          {stringifyPropValue({ propName: key, value: props[key], handler: onPropsChange })}
+        </>
+      );
+    });
+
+    return propsAsInputs[0];
+    // return Object.keys(props).reduce((acc, propName) => {
+    //   return `${acc}   ${propName}={${stringifyProp(props[propName])}}\n`;
+    // }, '');
   };
+
+  const onPropsChange = ({ target }) => {
+    const { propname, objectkey } = target.dataset;
+    const currentProp = props[propname];
+
+    const newProp = {
+      ...currentProp,
+      [objectkey]: target.textContent,
+    };
+
+    console.log('newProp:', newProp);
+
+    setProps({ [propname]: newProp });
+  };
+
+  useEffect(() => {
+    console.log('CHANGED!', props);
+  }, [props]);
 
   return (
     <>
       <pre>
-        <code>{`<${subject.displayName}\n` + `${displayActiveProps()}` + `/>`}</code>
+        <code>
+          {`<${subject.displayName}\n`} {displayActiveProps()} {`\n/>`}
+        </code>
       </pre>
-      <Subject {...exampleProps}>{exampleChildren}</Subject>
+      <Subject {...props}>{exampleChildren}</Subject>
     </>
   );
 };
